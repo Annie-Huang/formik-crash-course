d9edecdf9a30cede40d1872c6979773bf6c9429d
Annie Huang <annie.hn@gmail.com>
2025-06-23 16:59:56 +1000

    Create the useFormik hook in the <Signup> component

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useFormik } from 'formik';
 
+type Values = {
+  firstName: string;
+  lastName: string;
+  email: string;
+};
 const Signup = () => {
+  const initialValues: Values = {
+    firstName: '',
+    lastName: '',
+    email: '',
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit: () => {},
+  });
+
+  console.log('formik.values', formik.values);
+
   return (
     <form>
       <div className='input-container'>
@@ -9,6 +28,8 @@ const Signup = () => {
           name='firstName'
           type='text'
           placeholder='First Name'
+          onChange={formik.handleChange}
+          value={formik.values.firstName}
         />
       </div>
     </form>
